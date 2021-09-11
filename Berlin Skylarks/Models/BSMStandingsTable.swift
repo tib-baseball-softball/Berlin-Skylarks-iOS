97f6446991509ad9db39b71887bdce69202f1f47
David Battefeld <davidbattefeld@me.com>
2021-09-11 12:33:09 +0200

    start league table data model

--- Berlin Skylarks/Models/BSMStandingsTable.swift
+++ Berlin Skylarks/Models/BSMStandingsTable.swift
@@ -8,3 +8,34 @@
 import Foundation
 import SwiftUI
 
+struct LeagueTable: Hashable, Codable, Identifiable {
+    var id: Int
+    var league_id: String
+    var league_name: String
+    var season: Int
+    var rows: [Rows]
+    
+    struct Rows: Hashable, Codable {
+        var rank: String
+        var team_name: String
+        var short_team_name: String
+        var match_count: Int //those might be optionals!
+        var wins_count: Int
+        var losses_count: Int
+        var quota: String
+        var games_behind: String
+        var streak: String
+        
+//        var league_entry: LeagueEntry
+//    }
+//
+//    struct LeagueEntry: Hashable, Codable {
+//        var id: Int
+//        var team: Team
+//    }
+//
+//    struct Team: Hashable, Codable {
+//        var name: String
+//        var short_name: String
+    }
+}
