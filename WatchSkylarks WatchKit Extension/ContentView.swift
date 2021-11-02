9ed83f65d5946ca9a649c4526ba749b80a6e590b
David Battefeld <davidbattefeld@me.com>
2021-11-03 00:04:56 +0100

    design contentview for watchOS

--- WatchSkylarks WatchKit Extension/ContentView.swift
+++ WatchSkylarks WatchKit Extension/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Watch!")
-            .padding()
+        NavigationView {
+            List {
+                HStack {
+                    Image(systemName: "star")
+                        .foregroundColor(Color.accentColor)
+                    Text("Favorite Team")
+                }
+                HStack {
+                    Image(systemName: "newspaper")
+                        .foregroundColor(Color.accentColor)
+                    Text("News")
+                }
+                HStack {
+                    Image(systemName: "42.square")
+                        .foregroundColor(Color.accentColor)
+                    Text("Scores")
+                }
+                HStack {
+                    Image(systemName: "tablecells")
+                        .foregroundColor(Color.accentColor)
+                    Text("Standings")
+                }
+                HStack {
+                    Image(systemName: "person.3")
+                        .foregroundColor(Color.accentColor)
+                    Text("Players")
+                }
+                HStack {
+                    Image(systemName: "gearshape")
+                        .foregroundColor(Color.accentColor)
+                    Text("Settings")
+                }
+            }            
+            .navigationTitle("Home")
+        }
     }
 }
 
