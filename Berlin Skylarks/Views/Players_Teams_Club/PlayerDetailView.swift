d8f7fe86fa98811be338e00a2d5fbec759cbbe88
David Battefeld <davidbattefeld@me.com>
2022-12-09 20:47:46 +0100

    removed some prototyping data with localized strings to allow for l18n

--- Berlin Skylarks/Views/Players_Teams_Club/PlayerDetailView.swift
+++ Berlin Skylarks/Views/Players_Teams_Club/PlayerDetailView.swift
@@ -10,65 +10,66 @@ import SwiftUI
 struct PlayerDetailView: View {
     var body: some View {
         List {
-            Section(header: Text("Portrait")) {
-                HStack {
-                    Spacer()
-                    Image(systemName: "person.fill")
-                        .font(.system(size: 120))
-                    Spacer()
-                }
-            }
-            Section(header: Text("Main")) {
-                HStack {
-                    Text("Name")
-                    Spacer()
-                    Text("Jaro Bruders")
-                }
-                HStack {
-                    Text("Age")
-                    Spacer()
-                    Text("25")
-                }
-                HStack {
-                    Text("Club Member since")
-                    Spacer()
-                    Text("October 2015")
-                }
-            }
-            Section(header: Text("Game time")) {
-                HStack {
-                    Text("Team(s)")
-                    Spacer()
-                    Text("Team 1")
-                }
-                HStack {
-                    Text("Position")
-                    Spacer()
-                    Text("Pitcher, Corner Infielder") //that's not going to fit once a guy has a lot of positions
-                }
-                HStack {
-                    Text("Bats")
-                    Spacer()
-                    Text("Right")
-                }
-                HStack {
-                    Text("Throws")
-                    Spacer()
-                    Text("Right")
-                }
-            }
-            Section(header: Text("Licenses")) {
-                HStack {
-                    Text("Scorer")
-                    Spacer()
-                    Text("C")
-                }
-                HStack {
-                    Text("Umpire")
-                    Spacer()
-                    Text("A")
-                }
-            }
+            //commented due to localization issues
+//            Section(header: Text("Portrait")) {
+//                HStack {
+//                    Spacer()
+//                    Image(systemName: "person.fill")
+//                        .font(.system(size: 120))
+//                    Spacer()
+//                }
+//            }
+//            Section(header: Text("Main")) {
+//                HStack {
+//                    Text("Name")
+//                    Spacer()
+//                    Text("Jaro Bruders")
+//                }
+//                HStack {
+//                    Text("Age")
+//                    Spacer()
+//                    Text("25")
+//                }
+//                HStack {
+//                    Text("Club Member since")
+//                    Spacer()
+//                    Text("October 2015")
+//                }
+//            }
+//            Section(header: Text("Game time")) {
+//                HStack {
+//                    Text("Team(s)")
+//                    Spacer()
+//                    Text("Team 1")
+//                }
+//                HStack {
+//                    Text("Position")
+//                    Spacer()
+//                    Text("Pitcher, Corner Infielder") //that's not going to fit once a guy has a lot of positions
+//                }
+//                HStack {
+//                    Text("Bats")
+//                    Spacer()
+//                    Text("Right")
+//                }
+//                HStack {
+//                    Text("Throws")
+//                    Spacer()
+//                    Text("Right")
+//                }
+//            }
+//            Section(header: Text("Licenses")) {
+//                HStack {
+//                    Text("Scorer")
+//                    Spacer()
+//                    Text("C")
+//                }
+//                HStack {
+//                    Text("Umpire")
+//                    Spacer()
+//                    Text("A")
+//                }
+//            }
         }
         .listStyle(InsetGroupedListStyle())
         .navigationTitle("Player Details")
