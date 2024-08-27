2032889aa036c370dfb12eb18fab9319b6290cd6
David Battefeld <davidbattefeld@gmail.com>
2024-08-27 22:55:22 +0200

    polish: add version and build info also for macOS

--- Berlin Skylarks/Views/Settings/InfoView.swift
+++ Berlin Skylarks/Views/Settings/InfoView.swift
@@ -8,27 +8,32 @@
 import SwiftUI
 
 struct InfoView: View {
-
+#if os(iOS)
+    let appName = UIApplication.appName ?? "No App Name"
+    let version = UIApplication.appVersion ?? "No Version"
+    let build = UIApplication.appBuild ?? "No Build #"
+#endif
+    
+#if os(macOS)
+    let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String ?? "No App Name"
+    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "No Version"
+    let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "No Build #"
+#endif
+    
     var body: some View {
         List {
-            #if !os(watchOS) && !os(macOS)
+#if !os(watchOS)
             HStack {
                 Text("App Name:")
-                if let name = UIApplication.appName {
-                    Text(name)
-                }
+                Text(appName)
             }
             HStack {
                 Text("App Version:")
-                if let version = UIApplication.appVersion {
-                    Text(version)
-                }
+                Text(version)
             }
             HStack {
                 Text("Build Number:")
-                if let build = UIApplication.appBuild {
-                    Text(build)
-                }
+                Text(build)
             }
 #endif
         }
@@ -39,8 +44,6 @@ struct InfoView: View {
     }
 }
 
-struct InfoView_Previews: PreviewProvider {
-    static var previews: some View {
-        InfoView()
-    }
+#Preview {
+    InfoView()
 }
